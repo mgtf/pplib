3e70befde86a36519eb97ad509300e02b2b56823
tieum <tieum>
2002-01-23 15:18:12 +0000

    add confirmation when saving change.

--- list/list.js
+++ list/list.js
@@ -9,4 +9,14 @@
 	document[list].elements[i].checked = !document[list].elements[i].checked;
   }
 </script>
+<script language="JavaScript">
+function confirm_del()
+{
+  if (confirm('{confirm_mesg}'))
+  {
+    document.{id}.{id}_want_save.value = 1;
+    document.{id}.submit();
+  }
+}
+</script>
 <!-- end list.js -->
